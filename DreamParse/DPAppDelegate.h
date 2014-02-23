992293b286835c4de510ce39c29e1ad25b6fbb47
Liu Feng <gumeit@gmailcom>
2014-02-23 22:49:07 +0800

    Initial Commit

---
+++ DreamParse/DPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DPAppDelegate.h
+//  DreamParse
+//
+//  Created by Liu Feng on 14-2-23.
+//  Copyright (c) 2014年 Liu Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DreamParse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DreamParse
+//
+//  Created by Liu Feng on 14-2-23.
+//  Copyright (c) 2014年 Liu Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
+    }
+}
